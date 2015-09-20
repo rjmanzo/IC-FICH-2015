68a8c3bf8809aacaa589e11e5767b7c8e2da5dfa
Renato José Manzo <manzorenato@gmail.com>
2015-09-20 17:30:08 -0300

    Fix planificaicion

--- Guia 2/Ejercicio 3/particionar.m
+++ Guia 2/Ejercicio 3/particionar.m
@@ -1,34 +1,25 @@
-%Funcion Particionar
-%Recibe un archivo csv, la cantidad de parciones y el porcentaje de particion
-% Devuelve las particiones de entrenamiento y prueba
-function []= particionar(archivo,salida,particiones,p)
- v_trn=load(archivo); %%Cargo el archivo completo
- [n,m]=size(v_trn);
- for i=1:particiones
-     index=randperm(n);%Permuto el indice por cada nueva particion
-     
-     %%% Creo la matriz de entrenamiento parcial i
-     p_entrenamiento= zeros(floor(n*p),m); 
-     for j=1:(floor(p*n)) %desde 1 hasta el porcetaje p (0.8)
-     p_entrenamiento(j,:)=v_trn(index(j),:);
-     end
-     
-     %Creo la matriz con la particion de prueba parcial i
-     p_test= zeros(ceil(n*(1-p)),m); 
-     for j=1:ceil(n*(1-p)) 
-     p_test(j,:)=v_trn(index(floor(n*p)+j),:);%%Desde el ultimo valor del index hasta n del index
-     end  
-     
-     %Defino path de salidas
-     
-     %salida='../Datos/particiones/spheres2d10';
-     path_entrenamiento = strcat(salida,'_e_',num2str(i),'.csv');
-     path_test = strcat(salida,'_t_',num2str(i),'.csv');
-     %Escribo la particion i en el disco
-     csvwrite(path_entrenamiento,p_entrenamiento);
-     csvwrite(path_test,p_test);
- 
- end
-
+function Particionar(archivo,cp,porc)
+    %Parámetros:
+        %archivo    ->  ruta con datos a particionar
+        %cp         ->  cantidad de particiones
+        %porc       ->  porcentaje de patrones de prueba
+    
+    datos=load(strcat('Datos/',archivo,'.csv'));
+    archivoe=strcat(archivo,'_e');
+    archivop=strcat(archivo,'_p');
+    
+    [n,m]=size(datos);
+    cant_e=round((1-porc)*n);
+    Me=zeros(cant_e,m);
+    Mp=zeros(n-cant_e,m);
+    for i=1:cp
+        index=randperm(n);
+        ie=index(1:cant_e);
+        ip=index(cant_e+1:end);
+        Me=datos(ie,:);
+        Mp=datos(ip,:);
+        csvwrite(strcat('Datos/',archivoe,num2str(i),'.csv'),Me);
+        csvwrite(strcat('Datos/',archivop,num2str(i),'.csv'),Mp);
+    end
 end
 
