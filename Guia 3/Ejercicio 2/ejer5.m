1fd1f1dcc15042718a6af2f852531d2f8af19059
Renato José Manzo <manzorenato@gmail.com>
2015-10-07 17:06:13 -0300

    Lo que faltaba!!

--- Guia 3/Ejercicio 2/ejer5.m
+++ Guia 3/Ejercicio 2/ejer5.m
@@ -1,5 +1,11 @@
 clear all;
 close all;
+%Inicializacion de parametros
+x=10;
+r=[1 2 3 4 5 6 7];%Vector de correlacion. Mapea A--> B
+%r=[7 6 5 4 3 2 1];
+
+%Matriz de entrada M
 M=[-20 -20 -10 -5;
     -10 -5 -5 -2;
     -5 -2 -2 0;
@@ -7,8 +13,29 @@ M=[-20 -20 -10 -5;
     0 2 2 5;
     2 5 5 10;
     5 10 20 20];
-cota=20;
-paso=2;
 
-d= matris_activacion(cota, paso, M );
+%Matriz de salida S
+S=[-7 -5 -5 -3 ;
+-5 -3 -3 -1;
+-3 -1 -1 0;
+-1 0 0 1;
+0 1 1 3;
+1 3 3 5;
+3 5 5 7];
 
+%Calculo las activaciones para la matriz A
+a = grado_activacion(x,M);
+
+%Reordeno en funcion del indice
+mapeo=index_map(r,a);
+
+%Obtengo la salida del sistema mapeado y la escala de sus conjuntos
+[R,salida] = defuzzification_2(mapeo,S);
+
+%Grafico los conjuntos y la respuesta del sistema
+figure(1);
+graficar_conjunto(x,'Conjunto de entrada (M)',M);
+figure(2);
+graficar_conjunto(x,'Conjunto de salida (S)',S);
+figure(3);
+graficar_conjunto(salida,'Respuesta del sistema',R);
--- Guia 3/Ejercicio 2/graficar_conjunto.m
+++ Guia 3/Ejercicio 2/graficar_conjunto.m
@@ -1,5 +1,5 @@
 %Ejer2
-function [ output_args ] = graficar_conjunto(x,M)
+function [ output_args ] = graficar_conjunto(x,titulo,M)
 %UNTITLED6 Summary of this function goes here
 %   Detailed explanation goes here
 [n,m]=size(M);
@@ -7,6 +7,7 @@ function [ output_args ] = graficar_conjunto(x,M)
 if(m==4)
     hold on
     plot( M , [ 0 1 1 0 ] );
+    title(titulo);
     scatter(x,0,600,'.');
 else
     hold on
